3602e8741961581c6ce17aaed69f7c1f01cf6f02
ikamaci <kamacibrahim@gmail.com>
2022-11-08 00:32:34 +0300

    reverse warp added

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -3,13 +3,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-image1 = Image.open("paris/paris_a.jpg")
+image1 = Image.open("cv.jpg")
 image2 = Image.open("paris/paris_b.jpg")
 plt.imshow(image1)
-points1 = plt.ginput(5,timeout = 0, show_clicks=True)
+points1 = plt.ginput(2,timeout = 0, show_clicks=True)
 plt.imshow(image2)
-points2 = plt.ginput(5, timeout = 0, show_clicks=True)
+points2 = plt.ginput(10, timeout = 0, show_clicks=True)
 
-with open('paris1-2.npy', 'wb') as f:
+with open('paris1-2_10points2.npy', 'wb') as f:
     np.save(f, np.array(points1))
     np.save(f, np.array(points2))
