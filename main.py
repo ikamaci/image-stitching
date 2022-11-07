e109a7c5487089994a8c56ed68fe96afaf3fbc49
ikamaci <kamacibrahim@gmail.com>
2022-11-07 20:33:29 +0300

    warping added

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ import sys
 image1 = Image.open("paris/paris_a.jpg")
 image2 = Image.open("paris/paris_b.jpg")
 plt.imshow(image1)
-points1 = plt.ginput(5,timeout = 0, show_clicks=True)
+points1 = plt.ginput(10,timeout = 0, show_clicks=True)
 plt.imshow(image2)
-points2 = plt.ginput(5, timeout = 0, show_clicks=True)
+points2 = plt.ginput(10, timeout = 0, show_clicks=True)
 
-with open('paris1-2.npy', 'wb') as f:
+with open('paris1-2_10points.npy', 'wb') as f:
     np.save(f, np.array(points1))
     np.save(f, np.array(points2))
