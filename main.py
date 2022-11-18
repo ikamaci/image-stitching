84a9a9bc3fcb1abe453220d6b5332fb718ec846d
ikamaci <kamacibrahim@gmail.com>
2022-11-18 19:33:35 +0300

    add merge in one notebook

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-image1 = Image.open("paris/paris_a.jpg")
-image2 = Image.open("paris/paris_b.jpg")
+image1 = Image.open("paris/paris_b.jpg")
+image2 = Image.open("paris/paris_c.jpg")
+image1 = Image.open("mosaic2.jpg")
 plt.imshow(image1)
 points1 = plt.ginput(15,timeout = 0, show_clicks=True)
 plt.imshow(image2)
 points2 = plt.ginput(15, timeout = 0, show_clicks=True)
-
-with open('paris1-2_15points.npy', 'wb') as f:
+'''
+with open('paris2-3_15points.npy', 'wb') as f:
     np.save(f, np.array(points1))
     np.save(f, np.array(points2))
+'''
